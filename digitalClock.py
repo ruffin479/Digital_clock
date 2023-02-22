605d3925882e5b5c6d8c86cad4309d41d75ee197
mruffin <ruffinm0708@students.bowiestate.edu>
2023-02-22 17:50:19 -0500

    changed welcome message and background color

--- digitalClock.py
+++ digitalClock.py
@@ -3,5 +3,9 @@ from tkinter import Tk, Label
 window = Tk()
 window.title("Digital Clock")
 window.geometry("600x300")
+window.configure(bg="darkgreen")
+
+label = Label(window, text="Hello!", font=("Arial Black",78,"bold"), bg="darkgreen", fg="white")
+label.pack(pady=100)
 
 window.mainloop()
